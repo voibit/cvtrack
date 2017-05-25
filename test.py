651c6c311ea5908d89f2b36d80c0aeda30c44f74
voibit <krestean@gmail.com>
2017-05-25 19:53:14 +0200

    test

--- test.py
+++ test.py
@@ -1,16 +1,13 @@
 import cv2
 import numpy as np
 
-r1=0
-g1=0
-b1=0
-r2=0
-g2=0
-b2=0
+#color limits in hsv 
+lower=[0,0,0]
+upper=[0,0,0]
+
 def nothing(x):
     #print(x)
     pass
-
 
 cap = cv2.VideoCapture("./tst.mp4")
 
@@ -28,17 +25,12 @@ cv2.namedWindow('frame')
 cv2.namedWindow('lower')
 cv2.namedWindow('upper')
 cv2.namedWindow('mask')
+cv2.namedWindow('filtered')
 cv2.namedWindow('res')
-"""
-
-cv2.resizeWindow('frame', 500, 300)
-cv2.resizeWindow('mask', 500, 300)
-cv2.resizeWindow('res', 500, 300)
-
-"""
 
 cv2.moveWindow('frame', 0,0)
 cv2.moveWindow('mask', 0,400)
+cv2.moveWindow('filtered', 400,400)
 cv2.moveWindow('res', 0,800)
 cv2.moveWindow('lower', 450,0)
 cv2.moveWindow('upper', 755,0)
@@ -51,9 +43,9 @@ img2 = np.zeros((100,300,3), np.uint8)
 
 def sett(x):
     global r1, g1, g2
-    r1 = cv2.getTrackbarPos('R1','lower')
-    g1 = cv2.getTrackbarPos('G1','lower')
-    b1 = cv2.getTrackbarPos('B1','lower')
+    lower[0] = cv2.getTrackbarPos('R1','lower')
+    lower[1] = cv2.getTrackbarPos('G1','lower')
+    lower[2] = cv2.getTrackbarPos('B1','lower')
 
 
     img[:] = [b1,g1,r1]
@@ -62,9 +54,9 @@ def sett(x):
 
 def sett2(x):
     global r2, g2, b2
-    r2 = cv2.getTrackbarPos('R2','upper')
-    g2 = cv2.getTrackbarPos('G2','upper')
-    b2 = cv2.getTrackbarPos('B2','upper')
+    upper[0] = cv2.getTrackbarPos('R2','upper')
+    upper[1] = cv2.getTrackbarPos('G2','upper')
+    upper[2] = cv2.getTrackbarPos('B2','upper')
 
     img2[:] = [b2,g2,r2]
     cv2.imshow('upper',img2)
@@ -92,7 +84,7 @@ while(1):
 
     #slowdown
     k = cv2.waitKey()   
-    if k == 27:
+    if k == 27: #esc 
         break
 
 
@@ -102,42 +94,32 @@ while(1):
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # define range of blue color in HSV
-    lower_blue = np.array([r1,g1,b1])
-    upper_blue = np.array([r2,g2,b2])
-
-
-    #upper_blue = np.array([130,255,255])
-
-
     # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
-
+    mask = cv2.inRange(hsv, np.array(lower), np.array(upper))
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6,6))
 
-
     eroded = cv2.erode(mask, kernel, iterations = 2)
     dilated = cv2.dilate(eroded, kernel, iterations = 1)
-
     
-    # Bitwise-AND mask and original image
+
+    # show image where white in mask 
     res = cv2.bitwise_and(frame,frame, mask=mask)
  
+    # calculate position 
     moment= cv2.moments(dilated)
-
 
     if (moment['m00'] > 100000):
         posX = moment['m10']/moment['m00']
         posY = moment['m01']/moment['m00']
 
         print ("areal: "+str(moment['m00'])+" x:"+str(posX)+" y:"+str(posY))
+       	#draw circle to mark found position
         cv2.circle(frame,(int(posX),int(posY)),40,255)
         cv2.circle(frame,(int(posX),int(posY)),41,255)
 
 
-   
-
+    #show images
     cv2.imshow('frame',frame)
     cv2.imshow('mask',mask)
     cv2.imshow('filtered',dilated)
